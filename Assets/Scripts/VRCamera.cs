3e9574c69d4cdc4ba0292745538cdb342a20a7c6
xuyuejun-hci <mfm19201@bene.fit.ac.jp>
2020-12-29 15:36:09 +0900

    add vr camera

--- Assets/Scripts/VRCamera.cs
+++ Assets/Scripts/VRCamera.cs
@@ -5,9 +5,40 @@ using UnityEngine;
 public class VRCamera : MonoBehaviour
 {
     public Transform VR;
+    public Vector3 InitialPosition;
+    void OnMoveUp()
+    {
+        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y + 0.01f, transform.localPosition.z);
+    }
+    void OnMoveDown()
+    {
+        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y - 0.01f, transform.localPosition.z);
+    }
+    void OnMoveLeft()
+    {
+        transform.localPosition = new Vector3(transform.localPosition.x - 0.01f, transform.localPosition.y, transform.localPosition.z);
+    }
+    void OnMoveRight()
+    {
+        transform.localPosition = new Vector3(transform.localPosition.x + 0.01f, transform.localPosition.y, transform.localPosition.z);
+    }
+    void OnMoveForward()
+    {
+        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z + 0.01f);
+    }
+    void OnMoveBack()
+    {
+        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z - 0.01f);
+    }
+    void OnResetCamera()
+    {
+        print("test");
+        transform.localPosition = new Vector3(InitialPosition.x - VR.localPosition.x, InitialPosition.y - VR.localPosition.y, InitialPosition.z - VR.localPosition.z);
+    }
     // Start is called before the first frame update
     void Start()
     {
-        transform.localPosition = new Vector3(transform.localPosition.x - VR.localPosition.x, transform.localPosition.y - VR.localPosition.y, transform.localPosition.z - VR.localPosition.z);
+        InitialPosition = transform.localPosition;
+        transform.localPosition = new Vector3(InitialPosition.x - VR.localPosition.x, InitialPosition.y - VR.localPosition.y, InitialPosition.z - VR.localPosition.z);
     }
 }
